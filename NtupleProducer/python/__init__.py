3d3065c2e5fb9969067b50b715f3476ffa3a09d9
ccaillol <ccaillol@cern.ch>
2020-07-10 09:58:44 +0200

    first commit

--- NtupleProducer/python/__init__.py
+++
@@ -1,3 +0,0 @@
-#Automatically created by SCRAM
-import os
-__path__.append(os.path.dirname(os.path.abspath(__file__).rsplit('/B_mumupi_analyzer/NtupleProducer/',1)[0])+'/cfipython/slc7_amd64_gcc700/B_mumupi_analyzer/NtupleProducer')
