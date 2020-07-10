77c5559d39ecc42bb7089a4fff1053204704ed50
ccaillol <ccaillol@cern.ch>
2020-07-10 11:46:46 +0200

    bug fix

--- NtupleProducer/plugins/miniaod_ntuplizer.cc
+++ NtupleProducer/plugins/miniaod_ntuplizer.cc
@@ -95,13 +95,17 @@ class miniaod_ntuplizer : public edm::one::EDAnalyzer<edm::one::SharedResources>
       float genpt_1, geneta_1, genphi_1, genq_1=-1; // generated leading muon
       float genpt_2, geneta_2, genphi_2, genq_2=-1; // generated subleading muon
       float genpt_3, geneta_3, genphi_3, genq_3=-1; // generated pion 
-      float pt_1, pterr_1, eta_1, phi_1, q_1, softID_1, looseID_1, mediumID_1, iso_1=-1; // leading muon
-      float pt_2, pterr_2, eta_2, phi_2, q_2, softID_2, looseID_2, mediumID_2, iso_2=-1; // subleading muon
-      float pt_3, eta_3, phi_3, q_3, iso_3=-1; // pion 
+      float pt_1, pterr_1, eta_1, phi_1, q_1, looseID_1, mediumID_1=-1; // leading muon
+      float pt_2, pterr_2, eta_2, phi_2, q_2, looseID_2, mediumID_2=-1; // subleading muon
+      float pt_3, eta_3, phi_3, q_3=-1; // pion 
       float vtxprob_12, vtxprob_23, vtxprob_13, vtxprob_123=-1; // vertex fit probability
       int HLT_Mu7_IP4, HLT_Mu8_IP3, HLT_Mu8_IP5, HLT_Mu9_IP5, HLT_Mu9_IP6, HLT_Mu12_IP6=0; // B parking triggers
       int match_Mu7_IP4_1, match_Mu8_IP3_1, match_Mu8_IP5_1, match_Mu9_IP5_1, match_Mu9_IP6_1, match_Mu12_IP6_1=0;
       int match_Mu7_IP4_2, match_Mu8_IP3_2, match_Mu8_IP5_2, match_Mu9_IP5_2, match_Mu9_IP6_2, match_Mu12_IP6_2=0;
+      float validFraction_1, normalizedChi2_1, chi2LocalPosition_1, trkKink_1, segmentCompatibility_1, softMvaValue_1=-1;
+      float validFraction_2, normalizedChi2_2, chi2LocalPosition_2, trkKink_2, segmentCompatibility_2, softMvaValue_2=-1;
+      float m123=-1;
+
 };
 
 //
@@ -153,8 +157,6 @@ miniaod_ntuplizer::miniaod_ntuplizer(const edm::ParameterSet& iConfig):
    tr->Branch("eta_1", &eta_1, "eta_1/F");
    tr->Branch("phi_1", &phi_1, "phi_1/F");
    tr->Branch("q_1", &q_1, "q_1/F");
-   tr->Branch("iso_1", &iso_1, "iso_1/F");
-   tr->Branch("softID_1", &softID_1, "softID_1/F");
    tr->Branch("looseID_1", &looseID_1, "looseID_1/F");
    tr->Branch("mediumID_1", &mediumID_1, "mediumID_1/F");
    tr->Branch("pt_2", &pt_2, "pt_2/F");
@@ -162,15 +164,12 @@ miniaod_ntuplizer::miniaod_ntuplizer(const edm::ParameterSet& iConfig):
    tr->Branch("eta_2", &eta_2, "eta_2/F");
    tr->Branch("phi_2", &phi_2, "phi_2/F");
    tr->Branch("q_2", &q_2, "q_2/F");
-   tr->Branch("iso_2", &iso_2, "iso_2/F");
-   tr->Branch("softID_2", &softID_2, "softID_2/F");
    tr->Branch("looseID_2", &looseID_2, "looseID_2/F");
    tr->Branch("mediumID_2", &mediumID_2, "mediumID_2/F");
    tr->Branch("pt_3", &pt_3, "pt_3/F");
    tr->Branch("eta_3", &eta_3, "eta_3/F");
    tr->Branch("phi_3", &phi_3, "phi_3/F");
    tr->Branch("q_3", &q_3, "q_3/F");
-   tr->Branch("iso_3", &iso_3, "iso_3/F");
    tr->Branch("vtxprob_12", &vtxprob_12, "vtxprob_12/F");
    tr->Branch("vtxprob_23", &vtxprob_23, "vtxprob_23/F");
    tr->Branch("vtxprob_13", &vtxprob_13, "vtxprob_13/F");
@@ -193,6 +192,20 @@ miniaod_ntuplizer::miniaod_ntuplizer(const edm::ParameterSet& iConfig):
    tr->Branch("match_Mu9_IP5_2", &match_Mu9_IP5_2, "match_Mu9_IP5_2/I");
    tr->Branch("match_Mu9_IP6_2", &match_Mu9_IP6_2, "match_Mu9_IP6_2/I");
    tr->Branch("match_Mu12_IP6_2", &match_Mu12_IP6_2, "match_Mu12_IP6_2/I");
+   tr->Branch("m123", &m123, "m123/F");
+   tr->Branch("validFraction_1", &validFraction_1, "validFraction_1/F");
+   tr->Branch("normalizedChi2_1", &normalizedChi2_1, "normalizedChi2_1/F");
+   tr->Branch("chi2LocalPosition_1", &chi2LocalPosition_1, "chi2LocalPosition_1/F");
+   tr->Branch("trkKink_1", &trkKink_1, "trkKink_1/F");
+   tr->Branch("segmentCompatibility_1", &segmentCompatibility_1, "segmentCompatibility_1/F");
+   tr->Branch("softMvaValue_1", &softMvaValue_1, "softMvaValue_1/F");
+   tr->Branch("validFraction_2", &validFraction_2, "validFraction_2/F");
+   tr->Branch("normalizedChi2_2", &normalizedChi2_2, "normalizedChi2_2/F");
+   tr->Branch("chi2LocalPosition_2", &chi2LocalPosition_2, "chi2LocalPosition_2/F");
+   tr->Branch("trkKink_2", &trkKink_2, "trkKink_2/F");
+   tr->Branch("segmentCompatibility_2", &segmentCompatibility_2, "segmentCompatibility_2/F");
+   tr->Branch("softMvaValue_2", &softMvaValue_2, "softMvaValue_2/F");
+
 }
 
 
@@ -215,7 +228,7 @@ miniaod_ntuplizer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSet
 {
 
    // Event counter
-   //h_count->Fill(0.5,1.0);
+   h_count->Fill(0.5,1.0);
 
    // Get all collections
    Handle<GenParticleCollection> genParticles;
@@ -250,12 +263,21 @@ miniaod_ntuplizer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSet
      const GenParticle & p3 = (*genParticles)[i+2];
      const Candidate * mom = p.mother();
      if (fabs(p.pdgId())==13 and fabs(mom->pdgId())==521 and p.pdgId()==p2.pdgId()){ // find the generated muons
-	  mygenmuon_1.SetPtEtaPhiM(p.pt(),p.eta(),p.phi(),p.mass());
-	  mygenmuon_2.SetPtEtaPhiM(p2.pt(),p2.eta(),p2.phi(),p2.mass());
+	  if (p.pt()>p2.pt()){
+	    mygenmuon_1.SetPtEtaPhiM(p.pt(),p.eta(),p.phi(),p.mass());
+	    mygenmuon_2.SetPtEtaPhiM(p2.pt(),p2.eta(),p2.phi(),p2.mass());
+            genq_1=p.charge();
+            genq_2=p2.charge();
+	  }
+	  else{
+            mygenmuon_2.SetPtEtaPhiM(p.pt(),p.eta(),p.phi(),p.mass());
+            mygenmuon_1.SetPtEtaPhiM(p2.pt(),p2.eta(),p2.phi(),p2.mass());
+            genq_2=p.charge();
+            genq_1=p2.charge();
+          }
           mygenpion.SetPtEtaPhiM(p3.pt(),p3.eta(),p3.phi(),p3.mass());
-          genq_1=p.charge();
-          genq_2=p2.charge();
 	  genq_3=p3.charge();
+	  //std::cout<<"Gen particles: "<<p.pdgId()<<" ("<<p.pt()<<","<<p.eta()<<","<<p.phi()<<") ,"<<p2.pdgId()<<" ("<<p2.pt()<<","<<p2.eta()<<","<<p2.phi()<<") ,"<<p3.pdgId()<<" ("<<p3.pt()<<","<<p3.eta()<<","<<p3.phi()<<") ,"<<std::endl;
      }
      //std::cout<<p.pdgId()<<" ("<<mom->pdgId()<<"), ";
    }
@@ -281,11 +303,16 @@ miniaod_ntuplizer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSet
    reco::TrackRef trk1;
    reco::TrackRef trk2;
    int k=0;
+   //std::cout<<"reco muons: ";
    for (const pat::Muon &mu : *muons) {
       mytmpmu.SetPtEtaPhiM(mu.pt(),mu.eta(),mu.phi(),0.105);
-      if (mytmpmu.Pt()>2 and fabs(mytmpmu.Eta())<2.4 and mu.isGlobalMuon()) muon_indices.push_back(k);
+      if (mytmpmu.Pt()>2 and fabs(mytmpmu.Eta())<2.4 and mu.isGlobalMuon() and mu.isPFMuon()){ 
+	muon_indices.push_back(k);
+	//std::cout<<"("<<mytmpmu.Pt()<<","<<mytmpmu.Eta()<<","<<mytmpmu.Phi()<<"), ";
+      }
       k++; 
    }
+   //std::cout<<std::endl;
 
    // ######################
    // Get list of reco pions
@@ -296,11 +323,15 @@ miniaod_ntuplizer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSet
    vector<int> pion_indices;
    reco::Track trk3;
 
+   //std::cout<<"reco pions: ";
    for (unsigned int i = 0, n = pfs->size(); i < n; ++i) {
         const pat::PackedCandidate &pf = (*pfs)[i];
-	if (fabs(pf.pdgId()==211) and pf.pt()>2 and fabs(pf.eta())<2.4) pion_indices.push_back(i);
+	if (fabs(pf.pdgId())==211 and pf.pt()>2 and fabs(pf.eta())<2.4 and pf.hasTrackDetails()){ 
+	  pion_indices.push_back(i);
+          //std::cout<<"("<<pf.pt()<<","<<pf.eta()<<","<<pf.phi()<<"), ";
+	}
    }
-
+   //std::cout<<std::endl;
    // ############################################
    // Fill events with at least 2 muons and 1 pion
    // ############################################
@@ -310,7 +341,6 @@ miniaod_ntuplizer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSet
       // ###########################################
       // Vertex fitting and choice of best 3 objects
       // ###########################################
-      //int index_bestmu1, index_bestmu2, index_bestpion=-1;
       int index_bestmu1=muon_indices[0];
       int index_bestmu2=muon_indices[1];
       int index_bestpion=pion_indices[0];
@@ -323,11 +353,10 @@ miniaod_ntuplizer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSet
       for (unsigned int i_mu1=0; i_mu1<muon_indices.size()-1; ++i_mu1){
         for (unsigned int i_mu2=i_mu1+1; i_mu2<muon_indices.size(); ++i_mu2){
 	  for (unsigned int i_pi=0; i_pi<pion_indices.size(); ++i_pi){
-          //const pat::PackedCandidate &pf = (*pfs)[i];
             const pat::Muon &muon1 = (*muons)[muon_indices[i_mu1]];
             const pat::Muon &muon2 = (*muons)[muon_indices[i_mu2]];
 	    const pat::PackedCandidate &pion = (*pfs)[pion_indices[i_pi]];
-	    if (pion.hasTrackDetails() && !muon1.innerTrack().isNull() && !muon2.innerTrack().isNull()){ // all tracks exist and the objects are in the same area
+	    if (pion.hasTrackDetails() && !muon1.innerTrack().isNull() && !muon2.innerTrack().isNull()){ // all tracks exist 
               trk1 = muon1.innerTrack();
               trk2 = muon2.innerTrack();
               trk3 = pion.pseudoTrack();
@@ -346,13 +375,15 @@ miniaod_ntuplizer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSet
               }
 	    }
 	    else{ // bad candidate 
-	      fv_Prob = 0;
+	      fv_Prob = -99;
 	    }
+	    //std::cout<<muon1.pt()<<" "<<muon2.pt()<<" "<<pion.pt()<<" "<<fv_Prob<<std::endl;
             if (fv_Prob>old_prob) {
 	       index_bestmu1=muon_indices[i_mu1]; 
 	       index_bestmu2=muon_indices[i_mu2]; 
-	       index_bestpion=pion_indices[i_pi];}
+	       index_bestpion=pion_indices[i_pi];
 	       old_prob=fv_Prob;
+	    }
           }
         }
       }
@@ -378,17 +409,41 @@ miniaod_ntuplizer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSet
       phi_2=mymu2.phi();
       q_1=mymu1.charge();
       q_2=mymu2.charge();
-      //softID_1=mymu1.isSoftMuon();
-      //softID_2=mymu2.isSoftMuon();
       looseID_1=mymu1.isLooseMuon();
       looseID_2=mymu2.isLooseMuon();
       mediumID_1=mymu1.isMediumMuon();
       mediumID_2=mymu2.isMediumMuon();
 
+      validFraction_1=mymu1.innerTrack()->validFraction();
+      validFraction_2=mymu2.innerTrack()->validFraction();
+      normalizedChi2_1=mymu1.globalTrack()->normalizedChi2();
+      normalizedChi2_2=mymu2.globalTrack()->normalizedChi2();
+      chi2LocalPosition_1=mymu1.combinedQuality().chi2LocalPosition;
+      chi2LocalPosition_2=mymu2.combinedQuality().chi2LocalPosition;
+      trkKink_1=mymu1.combinedQuality().trkKink;
+      trkKink_2=mymu2.combinedQuality().trkKink;
+      segmentCompatibility_1=muon::segmentCompatibility(mymu1);
+      segmentCompatibility_2=muon::segmentCompatibility(mymu2);
+      softMvaValue_1=mymu1.softMvaValue();
+      softMvaValue_2=mymu2.softMvaValue();
+
       pt_3=mypion.pt();
       eta_3=mypion.eta();
       phi_3=mypion.phi();
       q_3=mypion.charge();
+
+      //std::cout<<"Chosen: ("<<mymu1.pt()<<" "<<mymu2.pt()<<" "<<mypion.pt()<<")"<<std::endl;
+
+      TLorentzVector myrecomuon1;
+      myrecomuon1.SetPtEtaPhiM(mymu1.pt(),mymu1.eta(),mymu1.phi(),mymu1.mass());
+      TLorentzVector myrecomuon2;
+      myrecomuon2.SetPtEtaPhiM(mymu2.pt(),mymu2.eta(),mymu2.phi(),mymu2.mass());
+      TLorentzVector myrecopion;
+      myrecopion.SetPtEtaPhiM(mypion.pt(),mypion.eta(),mypion.phi(),mypion.mass());
+      m123=(myrecomuon1+myrecomuon2+myrecopion).M();
+
+      //std::cout<<"Mass: "<<m123<<std::endl;
+      //std::cout<<"Vtx prob: "<<old_prob<<std::endl<<std::endl;
 
       // Compute vertex probabilities for best combination
       if (mypion.hasTrackDetails() && !mymu1.innerTrack().isNull() && !mymu2.innerTrack().isNull()){ // all tracks exist and the objects are in the same area
@@ -474,10 +529,6 @@ miniaod_ntuplizer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSet
 
       /*TLorentzVector mytrgobj;
       mytrgobj.SetPtEtaPhiM(0,0,0,0);
-      TLorentzVector myrecomuon1;
-      myrecomuon1.SetPtEtaPhiM(mymu1.pt(),mymu1.eta(),mymu1.phi(),mymu1.mass());
-      TLorentzVector myrecomuon2;
-      myrecomuon2.SetPtEtaPhiM(mymu2.pt(),mymu2.eta(),mymu2.phi(),mymu2.mass());
 
       for (pat::TriggerObjectStandAlone obj : *triggerObjects) {
          obj.unpackPathNames(names);
